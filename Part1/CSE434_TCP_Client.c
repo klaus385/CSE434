580f24cfbd2065a670d27f05d2a18c6826ba9483
kgosse412 <kgosse@asu.edu>
2015-09-08 15:55:42 -0700

    Only has header files.

---
+++ Part1/CSE434_TCP_Client.c
@@ -0,0 +1,29 @@
+/***********************************************
+ * Authors: Katie Gosse, Emily Faulkner
+ * Course: CSE 434, Computer Networks
+ * Semester: Fall 2015
+ * Project Part: 1
+ * Time Spent: ???
+ * File Name: CSE434_TCP_Client.c
+ * Description: ???
+ **********************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h> // Needed for socket system call
+#include <sys/types.h>  // Contains numerous data types used in system calls
+#include <netinet/in.h> // Needed for internet domain address constants/structs
+
+/***********************************************
+ * Client Program Steps:
+ * 1. Create a socket with socket() sys call
+ * 2. Connect the socket to the address of the
+ *    server using the connect() sys call
+ * 3. Send/receive data.. i.e. read(), write()
+ * 4. Close communication
+ **********************************************/
+
+int main (int argc, char *argv[])
+{
+	return 0;
+}
