806e3b2c6f1fa4628acfe3dfcb9a83472793b08e
emfalkne <emfalkne@asu.edu>
2015-09-09 22:05:51 -0700

    Put in Grovers Code

--- Part1/CSE434_TCP_Client.c
+++ Part1/CSE434_TCP_Client.c
@@ -9,10 +9,13 @@
  **********************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <sys/socket.h> // Needed for socket system call
 #include <sys/types.h>  // Contains numerous data types used in system calls
 #include <netinet/in.h> // Needed for internet domain address constants/structs
+#include <netdb.h>	// Needed to define the structure hostent
 
 /***********************************************
  * Client Program Steps:
@@ -23,7 +26,108 @@
  * 4. Close communication
  **********************************************/
 
+//This function is used whenever a system call fails. Displays a message about the error then aborts
+void error(const char *msg)
+{
+	perror(msg);
+	exit(0);
+}
+
+
 int main (int argc, char *argv[])
 {
+	//Variable declarations
+	int sockfd; //sockfd is a file descriptor
+	int portno; //portno stores the port number on which the server accepts the connections.
+	int n;
+
+	//variable server is a pointer to a structure of type hostent
+	/*
+	struct  hostent
+	{
+	  char    *h_name;        // official name of host
+	  char    **h_aliases;    // alias list
+	  int     h_addrtype;     // host address type
+	  int     h_length;       // length of address
+	  char    **h_addr_list;  // list of addresses from name server
+	  #define h_addr  h_addr_list[0]  // address, for backward compatiblity
+	};
+	*/
+
+	struct hostent *server;
+
+	char buffer[256];
+
+	portno = 8888; //atoi() function can be used to convert port number from string to int
+
+	//create socket
+	//takes 3 arguments - address domain, type of socet, protocol (zeror allows the OS to choose the appropriate protocols based on type of socket)
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+	{
+		error("ERROR OPENING SOCKET");
+	}
+
+	//gethostname() takes a name as an argument and returns a pointer to a hostent containing information about that host
+	//name is taken as argument from user
+	server = gethostname(argv[1]);
+
+	//If hostent structure is NULL, the system could not locate host name
+	if (server == NULL)
+	{
+		fprintf(stderr, "ERROR NO SUCH HOST\n"
+		exit(0);
+	}
+
+	//set server address buffer with xeros using bzero or memset
+	//two arguments - pointer to buffer and sizeof buffer
+	bzero((char*) &serv_addr, sizeof(serv_addr));
+
+	//sets the fields in serv_addr and for the address family
+    serv_addr.sin_family = AF_INET;
+
+    //comies length bytes from s1 to s2
+    bcopy((char *)server->h_addr,
+           (char *)&server_addr.sin_addr.s_addr,
+           server->h_length);
+
+    //contain the port number
+    serv_addr.sin_port = htons(portno);
+
+    //connect is callled by the client to establish connection to the server
+    //takes 3 argments: socket file descripter,address of the host that it wants to connect to, and the size of this address
+    if(connect(sockfd, (struck sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+    {
+        error("ERROR CONNECTING");
+    }
+    printf("Please enter the message: ");
+
+    //After a connection a client has succesfully connected to the server initilize buffer using bzero()
+    bzero(buffer,256);
+
+    //set buffer to the message entered on console at client end for a maximum of 255 characters
+    fgets(buffer, 255, stdin);
+
+    //write from the buffer into the socket
+    n = write(sockfd, buffer, strlent(buffer)):
+
+    //check if write was sucessful
+    if (n <0)
+    {
+        error("ERROR WRITING TO SOCKET"):
+    }
+
+    //server can read and write after connection has been established
+    //everything writen by client will be read by server and vice versa
+    bzero(buffer,256);
+    n = read(sockfd,buffer,255);
+    if (n < 0)
+    {
+        error("ERROR READIN FROM SOCKET");
+    }
+    printf("%s\n",buffer):
+
+    //close connections using file descriptrs
+	close(sockfd);
 	return 0;
 }
