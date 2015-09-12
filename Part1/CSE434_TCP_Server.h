c85cc0167497061ec6c04d6f24df8e33043a07e8
kgosse412 <kgosse@asu.edu>
2015-09-11 23:03:00 -0700

    Separated headers and function declaration of matching c file.

---
+++ Part1/CSE434_TCP_Server.h
@@ -0,0 +1,24 @@
+/*******************************************************************
+ * Authors: Katie Gosse, Emily Falkner
+ * Course: CSE 434, Computer Networks
+ * Semester: Fall 2015
+ * Project Part: 1
+ * Time Spent: ???
+ * File Name: CSE434_TCP_Server.c
+ * Description: ???
+ *****************************************************************/
+
+#include <stdio.h>      // used for input/output
+#include <stdlib.h>     // contains many standard C functions, like exit()
+#include <string.h>     // contains defintions for string manipulation
+#include <unistd.h>     // gives access to POSIX for sys calls
+#include <sys/types.h>  // contains definitions of numerous sys call data types
+#include <sys/socket.h> // includes structure definitions for socket types
+#include <netinet/in.h> // contains structures/constants for internet domain addresses
+
+#ifndef _CSE434_TCP_Server_H_
+#define _CSE434_TCP_Server_H_
+
+void error (char *msg);
+
+#endif /* _CSE434_TCP_Server_H_ */
